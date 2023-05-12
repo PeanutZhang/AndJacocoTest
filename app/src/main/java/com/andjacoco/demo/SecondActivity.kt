24a0deb3d8680e94caa3550e72aff12329c17f63
zyh <245502345@qq.com>
2023-05-12 17:22:40 +0800

    修改一些方法

--- app/src/main/java/com/andjacoco/demo/SecondActivity.kt
+++ app/src/main/java/com/andjacoco/demo/SecondActivity.kt
@@ -2,7 +2,9 @@ package com.andjacoco.demo
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import kotlin.random.Random
 
 class SecondActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,23 @@ class SecondActivity : BaseActivity() {
         SecondHello().sayHello()
 //        ThirdHello().sayHello()
         FourthHello().sayHello()
+
+        findViewById<Button>(R.id.btn_fk).setOnClickListener {
+            val fk = Random.nextInt(100)
+            Log.i("tag", "fk random: $fk")
+
+            if(fk < 10){
+                Log.i("tag", "fk < 10")
+
+            }else if(fk < 50){
+                Log.i("tag", "fk < 50")
+
+            }else if(fk < 80){
+                Log.i("tag", "fk < 80")
+            }else {
+                Log.i("tag", "log  ff>>> "+fk)
+            }
+        }
     }
 
     fun secondTest() {
